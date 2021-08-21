2516e29ec4c13081ca1b80b513604f7eec884dbd
SnowArs <Vnukovo4922>
2021-08-21 10:13:55 +0300

    поправил загрузку в правилиьную папку, почиситил дубли функций. добавил пояснений

--- VTB_NEW/func.py
+++ VTB_NEW/func.py
@@ -1,7 +1,9 @@
 from moex import ndfl_func
+from class_new import Calculations
+import math
 
 
-def culc(ticker, ndfl, profit_in_usd):
+def culc(ticker):
     print('начал выполнение culc функции')
     index_to_del = []
     for sale_row_number in ticker.index_sell_deals:
@@ -11,36 +13,24 @@ def culc(ticker, ndfl, profit_in_usd):
         diff_volume = bought_volume - sold_volume
         while not stop:
             if diff_volume == 0:
-                ndfl, profit_in_usd = profit_loss_calculation(ticker, 0, sale_row_number, sold_volume,
-                                                              ndfl, profit_in_usd, diff_volume)
-                # prof_loss_rur = ticker.df['RUB_sum'][sale_row_number] - \
-                #                 sold_volume * ticker.df['ROE'][ticker.index_sell_deals[0]] * \
-                #                 ticker.df['Price'][ticker.index_sell_deals[0]]
-                # prof_loss_usd = ticker.df['Sum'][sale_row_number] - \
-                #                 sold_volume * ticker.df['Price'][ticker.index_sell_deals[0]] - \
-                #                 ndfl_func(prof_loss_rur) / ticker.df['ROE'][sale_row_number]
-                # p_l_for_percentage_usd = sold_volume * ticker.df['Price'][ticker.index_sell_deals[0]] + \
-                #                          p_l_for_percentage_usd
+                ticker = profit_loss_calculation(ticker, 0, sale_row_number, sold_volume, diff_volume)
                 ticker.buy_volume_array = ticker.buy_volume_array[1:]
                 ticker.index_buy_deals = ticker.index_buy_deals[1:]
                 break
             elif diff_volume > 0:
-                ndfl, profit_in_usd = profit_loss_calculation(ticker, 0, sale_row_number, sold_volume,
-                                                              ndfl, profit_in_usd, diff_volume)
+                ticker = profit_loss_calculation(ticker, 0, sale_row_number, sold_volume, diff_volume)
                 ticker.buy_volume_array[0] = ticker.buy_volume_array[0] - sold_volume
                 break
             else:  # отрицательный объем
                 for i, buy_ind in enumerate(ticker.index_buy_deals):
                     if i == 0:
-                        ndfl, profit_in_usd = profit_loss_calculation(ticker, 1, sale_row_number, sold_volume,
-                                                                      ndfl, profit_in_usd, diff_volume, i)
+                        ticker = profit_loss_calculation(ticker, 1, sale_row_number, sold_volume, diff_volume, i)
                         negative_amount = diff_volume
                         ticker.buy_volume_array[0] = 0
                         index_to_del.append(i)
                     else:
                         diff = ticker.buy_volume_array[i] + negative_amount
-                        ndfl, profit_in_usd = profit_loss_calculation(ticker, 2, sale_row_number, -negative_amount,
-                                                                      ndfl, profit_in_usd, diff_volume, i)
+                        ticker = profit_loss_calculation(ticker, 2, sale_row_number, -negative_amount, diff_volume, i)
                         if diff < 0:
                             negative_amount = diff
                             index_to_del.append(i)
@@ -58,10 +48,11 @@ def culc(ticker, ndfl, profit_in_usd):
     if ticker.outstanding_volumes != sum(ticker.buy_volume_array):
         input(f"количество оставшихся бумаг {ticker.name} и полученных в результате подсчета не совпадают")
     print('закончил выполнение culc функции')
-    return ndfl, profit_in_usd
+    return ticker
 
 
-def profit_loss_calculation(ticker, option, sale_row_number, sold_volume, ndfl_, p_l_usd, diff_volume, i=0):
+def profit_loss_calculation(ticker, option, sale_row_number, sold_volume, diff_volume, i=0):
+    print('начал выполнение profit_loss_calculation функции')
     if option == 0:
         prof_loss_rur = ticker.df['RUB_sum'][sale_row_number] - \
                         sold_volume * ticker.df['ROE'][ticker.index_buy_deals[0]] * \
@@ -71,20 +62,92 @@ def profit_loss_calculation(ticker, option, sale_row_number, sold_volume, ndfl_,
                         ndfl_func(prof_loss_rur) / ticker.df['ROE'][sale_row_number]
 
     elif option == 1:
-        prof_loss_rur = (sold_volume + diff_volume)/sold_volume * ticker.df['RUB_sum'][sale_row_number] - \
+        prof_loss_rur = (sold_volume + diff_volume) / sold_volume * ticker.df['RUB_sum'][sale_row_number] - \
                         (sold_volume + diff_volume) * ticker.df['ROE'][i] * ticker.df['Price'][i]
         prof_loss_usd = (sold_volume + diff_volume) * ticker.df['Price'][sale_row_number] - (sold_volume + diff_volume) \
                         * ticker.df['Price'][i] - ndfl_func(prof_loss_rur) / ticker.df['ROE'][sale_row_number]
 
     else:
         prof_loss_rur = sold_volume * ticker.df['ROE'][sale_row_number] * ticker.df['Price'][sale_row_number] - \
-                        sold_volume * ticker.df.iloc[ticker.index_buy_deals[i]]['ROE'] *\
+                        sold_volume * ticker.df.iloc[ticker.index_buy_deals[i]]['ROE'] * \
                         ticker.df.iloc[ticker.index_buy_deals[i]]['Price']
-        prof_loss_usd = sold_volume * ticker.df['Price'][sale_row_number] -\
+        prof_loss_usd = sold_volume * ticker.df['Price'][sale_row_number] - \
                         sold_volume * ticker.df.iloc[ticker.index_buy_deals[i]]['Price'] - \
                         ndfl_func(prof_loss_rur) / ticker.df['ROE'][sale_row_number]
-        
-    ndfl_ = ndfl_func(prof_loss_rur) + ndfl_
-    p_l_usd = prof_loss_usd + p_l_usd
-    #    print(round(prof_loss_rur, 1), round(ndfl_, 1), round(p_l_usd, 1), round(p_l_for_percentage_usd, 1))
-    return ndfl_, p_l_usd
+
+    ticker.ndfl = ndfl_func(prof_loss_rur) + ticker.ndfl
+    ticker.profit_in_usd = prof_loss_usd + ticker.profit_in_usd
+
+    ticker.df['profit_rus'][sale_row_number] = prof_loss_rur
+    ticker.df['ndfl'][sale_row_number] = ticker.ndfl
+    ticker.df['prof_usd'][sale_row_number] = prof_loss_usd
+
+    print('закончил profit_loss_calculation')
+
+    return ticker
+
+
+def outstanding_volume_price(ticker):
+    print('start outstanding_volume_price')
+    sum_in_usd = 0
+    sum_in_rub = 0
+    if len(ticker.buy_volume_array) != 0:
+        for number, line in enumerate(ticker.index_buy_deals):
+            sum_in_usd = ticker.buy_volume_array[number] * ticker.df['Price'][line] + sum_in_usd
+            sum_in_rub = ticker.buy_volume_array[number] * ticker.df['Price'][line] * \
+                         ticker.df['ROE'][line] + sum_in_rub
+        ticker.average_price_usd = sum_in_usd / sum(ticker.buy_volume_array)
+        ticker.average_price_rub = sum_in_rub / sum(ticker.buy_volume_array)
+
+        try:
+            ticker.current_price = Calculations.get_current_price_usd(ticker.name)
+            ticker.profit_for_outstanding_volumes = (ticker.current_price - ticker.average_price_usd) \
+                                                    * ticker.outstanding_volumes
+            ticker.total_profit = int(ticker.profit_in_usd + ticker.profit_for_outstanding_volumes - \
+                                      ndfl_func(ticker.profit_for_outstanding_volumes))
+            ticker.current_price = round(Calculations.get_current_price(ticker.name), 1)
+        except IndexError:
+            ticker.profit_for_outstanding_volumes = 'N/A'
+            ticker.total_profit = 'N/A'
+            ticker.current_price = 'N/A'
+
+    else:
+        ticker.average_price_usd = 0
+        ticker.profit_for_outstanding_volumes = 0
+        ticker.average_price_rub = 0
+        ticker.total_profit = int(ticker.profit_in_usd)
+        ticker.current_price = Calculations.get_current_price(ticker.name)
+        print('закончил outstanding_volume_price')
+
+    return ticker
+
+
+def rub_securities_processing(ticker):
+    print('start rub_securities_processing')
+    if len(ticker.name) > 4:
+        board = 'TQCB'
+        market = 'bonds'
+        ratio = 10
+    else:
+        board = 'TQBR'
+        market = 'shares'
+        ratio = 1
+
+    ticker.average_buy = ticker.buy_sum_for_rub_securities / ticker.total_buy
+    ticker.average_sell = ticker.sell_sum_for_rub_securities / ticker.total_sell
+    ticker.current_price = Calculations.get_current_price_rur(ticker, board, market, ratio)
+    if ticker.outstanding_volumes == 0:
+        ticker.prof_loss_for_sold_securities = ticker.sell_sum_for_rub_securities - ticker.buy_sum_for_rub_securities
+        ticker.profit_for_outstanding_volumes = 0
+    elif math.isnan(ticker.average_sell):
+        ticker.prof_loss_for_sold_securities = 0
+        ticker.average_sell = 0
+        ticker.profit_for_outstanding_volumes = (ticker.current_price - ticker.average_buy) * ticker.outstanding_volumes
+    else:
+        ticker.prof_loss_for_sold_securities = ticker.sell_sum_for_rub_securities - ticker.total_sell * ticker.average_buy
+        ticker.profit_for_outstanding_volumes = (ticker.current_price - ticker.average_buy) * ticker.outstanding_volumes
+
+    ticker.total_profit = ticker.prof_loss_for_sold_securities + ticker.profit_for_outstanding_volumes
+    print('закончил rub_securities_processing')
+
+    return ticker
--- VTB_NEW/main_new.py
+++ VTB_NEW/main_new.py
@@ -1,237 +1,155 @@
 import warnings
-from prettytable import PrettyTable
-from prettytable import ALL
 from moex import *
 import class_new
 import os.path
+from func import culc, outstanding_volume_price, rub_securities_processing
 
 warnings.filterwarnings('ignore')
 
+""""" 
+1/ создаются таблицы для вывода в более читаемом выводе, чем вывод датафрейма
+2/ 
+"""""
+def main_func(full_list_of_securities, df, broker):
+    from prettytable import PrettyTable
+    from prettytable import ALL
+    full_ndfl = 0
+    full_prof_loss_usd = 0
+    full_potential_profit = 0
+    full_potential_profit_rus = 0
+    # сознадние таблицы вывода
+    mytable = PrettyTable()  # иностранные бумаги
+    mytable_rus = PrettyTable()
+    mytable_rus.hrules = ALL
+    # имена полей таблицы
+    field_names = ['Тикер', 'Куплено', 'Продано', 'Остаток', 'НДФЛ, РУБ', 'Прибыль в USD',
+                   'средняя цена', 'текущая цена', 'потенциальная прибыль', 'прибыль всех бумаг']
+    mytable.field_names = field_names
+    mytable_rus.field_names = field_names[0:4] + ['заф прибыль РУБ'] + field_names[7:]
 
-def profit_loss_calculation(list_):
-    df_ = list_[0]
-    option_ = list_[1]
-    ndfl_ = list_[2]
-    p_l_usd = list_[3]
-    p_l_for_percentage_usd = list_[4]
+    """"" 
+    2/ по каждой бумаге в портфеле производится подсчет показателей прибыльности
+    """""
+    for security in full_list_of_securities:
+        df_for_particular_security = df.loc[df.iloc[:, 1] == security].reset_index(drop=True)
+        ticker = class_new.Ticker(df_for_particular_security, broker)
+        ticker.df = pd.concat([ticker.df, pd.DataFrame(columns=['profit_rus', 'ndfl', 'prof_usd'])])
 
-    if option_ == 0:
-        prof_loss_rur = df_['RUB_sum'][index] - sale_volume * df_['ROE'][index_arr[0]] * df_['Price'][index_arr[0]]
-        prof_loss_usd = df_['Sum'][index] - sale_volume * df_['Price'][index_arr[0]] - \
-                        ndfl_func(prof_loss_rur) / df_['ROE'][index]
-        p_l_for_percentage_usd = sale_volume * df_['Price'][index_arr[0]] + p_l_for_percentage_usd
-    elif option_ == 1:
-        prof_loss_rur = (sale_volume + close) * df_['ROE'][index] * df_['Price'][index] - \
-                        (sale_volume + close) * df_['ROE'][buy_ind] * df_['Price'][buy_ind]
-        prof_loss_usd = (sale_volume + close) * df_['Price'][index] - (sale_volume + close) \
-                        * df_['Price'][buy_ind] - ndfl_func(prof_loss_rur) / df_['ROE'][index]
-        p_l_for_percentage_usd = (sale_volume + close) * df_['Price'][index_arr[0]] + p_l_for_percentage_usd
-    else:
-        prof_loss_rur = buy_arr[i] * df_['ROE'][index] * df_['Price'][index] - \
-                        buy_arr[i] * df_['ROE'][buy_ind] * df_['Price'][buy_ind]
-        prof_loss_usd = buy_arr[i] * df_['Price'][index] - buy_arr[i] * df_['Price'][buy_ind] - \
-                        ndfl_func(prof_loss_rur) / df_['ROE'][index]
-        p_l_for_percentage_usd = buy_arr[i] * df_['Price'][index_arr[0]] + p_l_for_percentage_usd
-    ndfl_ = ndfl_func(prof_loss_rur) + ndfl_
-    p_l_usd = prof_loss_usd + p_l_usd
-    #    print(round(prof_loss_rur, 1), round(ndfl_, 1), round(p_l_usd, 1), round(p_l_for_percentage_usd, 1))
-    return ndfl_, p_l_usd, p_l_for_percentage_usd
+        # указание на ошибку если остаток акций отрицательный
+        if ticker.outstanding_volumes < 0:
+            print(f'похоже в позиции {ticker.name} проблемы с вычислениями, так как остаток отрицательный')
+            continue
+        # обработка рублевых бумаг
+        if ticker.currency == 'RUR':
+            ticker = rub_securities_processing(ticker)
 
+            mytable_rus.add_row([ticker.name,
+                                 ticker.total_buy,
+                                 ticker.total_sell,
+                                 ticker.outstanding_volumes,
+                                 int(ticker.prof_loss_for_sold_securities_rus),
+                                 ticker.current_price,
+                                 int(ticker.profit_for_outstanding_volumes_rus),
+                                 int(ticker.total_profit_rus)])
 
-df = pd.read_excel('сделки_ВТБ.xls', sheet_name='DealOwnsReport', header=3)
-df = df.loc[df['Тип сделки'] == 'Клиентская'].reset_index(drop=True)
-full_list_of_securities = df['Код инструмента'].unique().tolist()
-
-# группировка
-df = df.groupby(['Дата вал.', 'Код инструмента', 'B/S', 'Валюта']).agg(
-    Price=pd.NamedAgg(column='Цена', aggfunc='mean'),
-    Volume=pd.NamedAgg(column='Кол-во', aggfunc='sum'),
-    NKD=pd.NamedAgg(column='НКД', aggfunc='sum'),
-    Sum=pd.NamedAgg(column='Объем', aggfunc='sum')
-)
-
-df.reset_index(drop=False, inplace=True)
-# создание двух новых колонок с заполнением
-df = pd.concat([df, pd.DataFrame(columns=['ROE_index'])])
-for index, row in df.iterrows():
-    df['ROE_index'][index] = row['Дата вал.'].strftime('%Y-%m-%d') + '_' + row['Валюта']
-
-# заполнение ROE/RUB_sum
-if os.path.exists('roe_table.csv'):
-    df_roe = pd.read_csv('roe_table.csv', usecols=['ROE_index', 'ROE'])
-    # df_roe['Дата вал.'] = df_roe['Дата вал.'].apply(pd.Timestamp)
-    df = df.merge(df_roe, how='left', left_on='ROE_index', right_on='ROE_index')
-    df.loc[df['Валюта'] == 'RUR', 'ROE'] = 1
-    # df_with_missed_roe
-    if df.loc[df['ROE'].isna()].empty:
-        print('ROE по всем датам проставленно')
-    else:
-        for index, row in df.loc[df['ROE'].isna()].iterrows():
-            df['ROE'][index] = fill_roe(row['Дата вал.'], row['Валюта'], df['Дата вал.'].min())
-
-else:
-    for index, row in df.iterrows():
-        df = pd.concat([df, pd.DataFrame(columns=['ROE'])])
-        df['ROE'][index] = fill_roe(row['Дата вал.'], row['Валюта'], df['Дата вал.'].min())
-df_roe = df.loc[(df['ROE'] != 1) & (df['Дата вал.'] < dt.datetime.today().strftime('%Y-%m-%d')), ['ROE_index', 'ROE']]
-df_roe = df_roe.drop_duplicates('ROE_index', ignore_index=True)
-df_roe.to_csv('roe_table.csv', index=False)
-
-df['RUB_sum'] = df['Sum'] * df['ROE']
-df = df.astype({'ROE': 'float', 'RUB_sum': 'float'})
-
-full_ndfl = 0
-full_prof_loss_usd = 0
-full_profit_loss_percentage = 0
-Full_potential_profit = 0
-Full_potential_profit_rus = 0
-
-# сознадние таблицы вывода
-mytable = PrettyTable()
-# mytable.hrules = ALL
-mytable_rus = PrettyTable()
-mytable_rus.hrules = ALL
-# имена полей таблицы
-field_names = ['Тикер', 'Куплено', 'Продано', 'Остаток', 'НДФЛ, РУБ', 'Прибыль в USD', 'Прибыль в usd в %',
-               'средняя цена', 'текущая цена', 'потенциальная прибыль', 'прибыль всех бумаг']
-mytable.field_names = field_names
-mytable_rus.field_names = field_names[0:4] + ['заф прибыль РУБ'] + field_names[7:]
-
-for security in full_list_of_securities:
-    df_for_particular_security = df.loc[df['Код инструмента'] == security].reset_index(drop=True)
-    ticker = class_new.Ticker(df_for_particular_security)
-    prof_loss_for_sold_securities = 0
-    buy_arr = []
-    index_arr = []
-    index_to_del = []
-    ndfl = 0
-    prof_loss_usd = 0
-    profit_in_usd = 0
-    profit_for_percentage_calculation_usd = 0
-    total_profit_rus = 0
-    total_profit = 0
-    # указание на ошибку если остаток акций отрицательный
-    if ticker.outstanding_volumes < 0:
-        print(f'похоже в позиции {ticker.name} проблемы с вычислениями, так как остаток отрицательный')
-        continue
-    # обработка рублевых бумаг
-    if ticker.currency == 'RUR':
-        prof_loss_for_sold_securities_rus, average_buy, profit_for_outstanding_volumes_rus, total_profit_rus = \
-            class_new.Calculations.rub_securities_processing(ticker, df_for_particular_security)
-        mytable_rus.add_row([ticker.name,
+        # блок вычисления прибыли и убытков по бумаге в USD и EUR
+        else:
+            # вычесление прибыли и убытков
+            ticker = culc(ticker)
+            #сохранение для более простой проверки правильности расчетов
+            ticker.df.to_excel(f'VTB_NEW\\calc\\{ticker.name}.xls')
+            # вычисление средней цены  оставшихся бумаг в рублях и валюте
+            ticker = outstanding_volume_price(ticker)
+            # заполнение таблицы
+            mytable.add_row([ticker.name,
                              ticker.total_buy,
                              ticker.total_sell,
                              ticker.outstanding_volumes,
-                             int(prof_loss_for_sold_securities_rus),
-                             round(average_buy, 1),
-                             current_price,
-                             int(profit_for_outstanding_volumes_rus),
-                             int(total_profit_rus)])
+                             int(ticker.ndfl),
+                             int(ticker.profit_in_usd),
+                             round(ticker.average_price_usd, 1),
+                             round(ticker.current_price, 1),
+                             round(ticker.profit_for_outstanding_volumes, 1),
+                             int(ticker.total_profit)])
 
-    # блок вычисления прибыли и убытков по бумаге в USD и EUR
+        full_ndfl = full_ndfl + ticker.ndfl
+        full_prof_loss_usd = full_prof_loss_usd + ticker.profit_in_usd
+        full_potential_profit = full_potential_profit + ticker.total_profit
+        full_potential_profit_rus = full_potential_profit_rus + ticker.total_profit_rus
+
+    return ticker, mytable, mytable_rus, full_ndfl, full_prof_loss_usd, full_potential_profit, full_potential_profit_rus
+
+
+""""" 
+создание базы данных по курсам ЦБ для определнных валют и на определнную датуза, проверка новых дат,
+заполнение колонок  ROE/RUB_sum
+"""""
+
+
+def filling_roe(_df, date_column, currency_column):
+    _df = pd.concat([_df, pd.DataFrame(columns=['ROE_index'])])
+    for _index, _row in _df.iterrows():
+        _df['ROE_index'][_index] = _row[date_column].strftime('%Y-%m-%d') + '_' + _row[currency_column]
+
+    if os.path.exists('roe_table.csv'):
+        df_roe = pd.read_csv('roe_table.csv', usecols=['ROE_index', 'ROE'])
+        _df = _df.merge(df_roe, how='left', left_on='ROE_index', right_on='ROE_index')
+        _df.loc[_df.iloc[:, currency_column] == 'RUR', 'ROE'] = 1
+        if _df.loc[_df['ROE'].isna()].empty:
+            print('ROE по всем датам проставленно')
+        else:
+            for ind, line in _df.loc[_df['ROE'].isna()].iterrows():
+                _df['ROE'][ind] = fill_roe(line[date_column], line[currency_column], _df.iloc[:, date_column].min())
+                print(ind)
     else:
-        for index, deal in df_for_particular_security.iterrows():
-            if deal['B/S'] == 'Покупка':
-                buy_arr.append(deal['Volume'])
-                index_arr.append(index)
-            else:
-                sale_volume = deal['Volume']
-                buy_volume = buy_arr[0]
-                close = buy_volume - sale_volume
-                option = 0
-                list_for_prof_los_func = [df_for_particular_security, option, ndfl, profit_in_usd,
-                                                  profit_for_percentage_calculation_usd]
-                if close == 0:
-                    ndfl, profit_in_usd, profit_for_percentage_calculation_usd \
-                        = profit_loss_calculation(list_for_prof_los_func)
-                    buy_arr.pop(0)
-                    index_arr.pop(0)
+        for ind, line in _df.iterrows():
+            _df = pd.concat([_df, pd.DataFrame(columns=['ROE'])])
+            _df['ROE'][line] = fill_roe(line[date_column], line[currency_column], _df.iloc[:, date_column].min())
+    _df2 = _df.loc[(_df['ROE'] != 1) & (_df.iloc[:, date_column] < dt.datetime.today().strftime('%Y-%m-%d')),\
+                  ['ROE_index', 'ROE']]
+    _df2 = _df.drop_duplicates('ROE_index', ignore_index=True)
+    df_roe = df_roe.append(_df2)
+    df_roe = df_roe.drop_duplicates('ROE_index', ignore_index=True)
+    df_roe.to_csv('roe_table.csv', index=False)
 
-                elif close > 0:
-                    ndfl, profit_in_usd, profit_for_percentage_calculation_usd \
-                        = profit_loss_calculation(list_for_prof_los_func)
-                    buy_arr[0] = buy_arr[0] - sale_volume
-                else:
-                    for i, buy_ind in enumerate(index_arr):
-                        if i == 0:
-                            option = 1
-                            ndfl, profit_in_usd, profit_for_percentage_calculation_usd \
-                                = profit_loss_calculation(list_for_prof_los_func)
-                            close_diff = close
-                            index_to_del.append(i)
-                        else:
-                            diff = buy_arr[i] + close_diff
-                            option = 2
-                            ndfl, profit_in_usd, profit_for_percentage_calculation_usd \
-                                = profit_loss_calculation(list_for_prof_los_func)
-                            if diff < 0:
-                                close_diff = diff
-                                index_to_del.append(i)
-                            elif diff > 0:
-                                buy_arr[i] = buy_arr[i] - diff
-                                break
-                            else:
-                                index_to_del.append(i)
-                                break
-                    buy_arr = buy_arr[len(index_to_del):]
-                    index_arr = index_arr[len(index_to_del):]
+    _df['RUB_sum'] = _df['Sum'] * _df['ROE']
+    _df = _df.astype({'ROE': 'float', 'RUB_sum': 'float'})
+    return _df
 
-        # вычисление средней цены  оставшихся бумаг в рублях и валюте
-        sum_in_usd = 0
-        sum_in_rub = 0
-        if len(buy_arr) != 0:
-            for number, line in enumerate(index_arr):
-                sum_in_usd = buy_arr[number] * df_for_particular_security['Price'][line] + sum_in_usd
-                sum_in_rub = buy_arr[number] * df_for_particular_security['Price'][line] * \
-                             df_for_particular_security['ROE'][line] + sum_in_rub
-            average_price_usd = sum_in_usd / sum(buy_arr)
-            average_price_rub = sum_in_rub / sum(buy_arr)
 
-            try:
-                profit_for_outstanding_volumes = (get_current_price(ticker.name) - average_price_usd) \
-                                                 * ticker.outstanding_volumes
-                total_profit = int(profit_in_usd + profit_for_outstanding_volumes -\
-                                   ndfl_func(profit_for_outstanding_volumes))
-                current_price = round(get_current_price(ticker.name), 1)
-            except IndexError:
-                profit_for_outstanding_volumes = 'N/A'
-                total_profit = 'N/A'
-                current_price = 'N/A'
+"""""
+производится преобразования файла с сайта ВТБ в датафрейм, с последующей отправкой в main_func
+"""""
 
-        else:
-            average_price_usd = 0
-            profit_for_outstanding_volumes = 0
-            average_price_rub = 0
-            total_profit = int(profit_in_usd)
-            current_price = 0
 
-        if profit_for_percentage_calculation_usd != 0:
-            p_l_for_percentage_calc = round(profit_in_usd / profit_for_percentage_calculation_usd * 100, 1)
-        else:
-            p_l_for_percentage_calc = round(profit_in_usd / 1 * 100, 1)
+def main():
 
-        mytable.add_row([ticker.name,
-                         ticker.total_buy,
-                         ticker.total_sell,
-                         ticker.outstanding_volumes,
-                         int(ndfl),
-                         int(profit_in_usd),
-                         p_l_for_percentage_calc,
-                         round(average_price_usd, 1),
-                         current_price,
-                         round(profit_for_outstanding_volumes, 1),
-                         int(total_profit)])
+    df = pd.read_excel('сделки_ВТБ.xls', sheet_name='DealOwnsReport', header=3)
+    df = df.loc[df['Тип сделки'] == 'Клиентская'].reset_index(drop=True)
+    full_list_of_securities = df['Код инструмента'].unique().tolist()
 
-    full_ndfl = full_ndfl + ndfl
-    full_prof_loss_usd = full_prof_loss_usd + profit_in_usd
-    full_profit_loss_percentage = full_profit_loss_percentage + profit_for_percentage_calculation_usd
-    Full_potential_profit = Full_potential_profit + total_profit
-    Full_potential_profit_rus = Full_potential_profit_rus + total_profit_rus
-print()
-print(mytable_rus)
-print(f'по рублевым бумагам прибыль: {int(Full_potential_profit_rus)}')
-print()
-print(mytable)
-print(f'НДФЛ по всем бумагам в РУБ:  {int(full_ndfl)},\
-        общая прибыль по всем бумагам в USD: {int(Full_potential_profit)},\
-         общая прибыль в % {round(full_prof_loss_usd / full_profit_loss_percentage * 100, 1)} ')
+    # так как сделики в течении дня происходят разными строкам требуется группировка
+    df = df.groupby(['Дата вал.', 'Код инструмента', 'B/S', 'Валюта']).agg(
+        Price=pd.NamedAgg(column='Цена', aggfunc='mean'),
+        Volume=pd.NamedAgg(column='Кол-во', aggfunc='sum'),
+        NKD=pd.NamedAgg(column='НКД', aggfunc='sum'),
+        Sum=pd.NamedAgg(column='Объем', aggfunc='sum')
+    )
+    df.reset_index(drop=False, inplace=True)
+    df = filling_roe(df, 0, 3) #заполнение курса ЦБ по каждой из операций
+    broker = 'VTB'
+
+    ticker, mytable, mytable_rus, full_ndfl, full_prof_loss_usd, full_potential_profit, full_potential_profit_rus = \
+        main_func(full_list_of_securities, df, broker)
+
+    print()
+    print(mytable_rus)
+    print(f'по рублевым бумагам прибыль: {int(full_potential_profit_rus)}')
+    print()
+    print(mytable)
+    print(f'НДФЛ по всем бумагам в РУБ:  {int(full_ndfl)},'
+          f'общая прибыль по всем бумагам в USD: {int(full_potential_profit)}')
+
+
+if __name__ == '__main__':
+    main()
